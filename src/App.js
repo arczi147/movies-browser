2cbfb9200785f5ac5972117618290086f542e88f
Michal Owsiak <micowsiak@gmail.com>
2023-10-31 12:13:42 +0100

    global Wrapper added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
 import NavigationBar from "./common/NavigationBar";
 import Movies from "./features/movieList/Movies";
+import { Wrapper } from "./styled";
 
 function App() {
   return (
     <HashRouter>
       <NavigationBar />
       <Switch>
-        <Route path="/movies">
-          <Movies />
-        </Route>
-        <Route path="/">
-          <Redirect to="/movies" />
-        </Route>
+        <Wrapper>
+          <Route path="/movies">
+            <Movies />
+          </Route>
+          <Route path="/">
+            <Redirect to="/movies" />
+          </Route>
+        </Wrapper>
       </Switch>
     </HashRouter>
   );
---
+++ src/styled.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  max-width: 1368px;
+  margin: 0 auto;
+  padding: 0 26px;
+`;
