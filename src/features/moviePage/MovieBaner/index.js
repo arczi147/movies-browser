c4dec9b9bf212badf8c28f5d2a22e46a3b9c9635
Michał Owsiak <micowsiak@gmail.com>
2023-11-04 03:11:19 +0100

    movie info on baner added

--- src/features/moviePage/MovieBaner/index.js
+++ src/features/moviePage/MovieBaner/index.js
@@ -1,16 +1,37 @@
 import { GlobalWrapper } from "../../../common/GlobalWrapper/styled";
-import { Background, BanerContainer, Baner, Shadow } from "./styled";
+import { 
+  Background,
+  BanerContainer,
+  Shadow,
+  Baner,
+  MovieInfo,
+  Title,
+  Stats,
+  RatingIcon,
+  Rating,
+  RatingScale,
+  Votes
+} from "./styled";
 import baner from "./baner.png";
 import shadow from "../../../images/shadow.png"
 
 const MovieBaner = () => (
   <Background>
-    <BanerContainer>
-      <Shadow src={shadow} />
-      <GlobalWrapper>
-        <Baner src={baner} />
-      </GlobalWrapper>
-    </BanerContainer>
+    <GlobalWrapper>
+      <BanerContainer>
+        <Shadow src={shadow} alt="Shadow Frame" />
+        <Baner src={baner} alt="Baner" />
+        <MovieInfo>
+          <Title>Mulan long title</Title>
+          <Stats>
+            <RatingIcon />
+            <Rating>7,8</Rating>
+            <RatingScale>/ 10</RatingScale>
+            <Votes>335 votes</Votes>
+          </Stats>
+        </MovieInfo>
+      </BanerContainer>
+    </GlobalWrapper>
   </Background>
 );
 
