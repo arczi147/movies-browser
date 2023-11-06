b4b574f8690bfb277567cf40a2d4b943e5a5eea9
Angelika Mallek <angelika.mallek03@gmail.com>
2023-11-06 14:31:26 +0100

    changed props in MovieTitleDetails

--- src/features/moviePage/MovieTileDetails/index.js
+++ src/features/moviePage/MovieTileDetails/index.js
@@ -22,7 +22,7 @@ import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { getMovieById } from "../../movieSlice";
 
-const MovieTileDetails = ({ id, poster, title, year, genre, rating, votes }) => {
+const MovieTileDetails = ({ title, year, poster, movieDate, movieDateText, genre, rating, ratingMax, votes, description }) => {
 
     const [isWideScreen, setIsWideScreen] = useState(window.innerWidth > 979);
 
@@ -47,8 +47,8 @@ const MovieTileDetails = ({ id, poster, title, year, genre, rating, votes }) => 
                 <Poster src={poster} alt="Poster" />
                     {/* <NoPoster src={noPoster} alt="Logo" /> */}
                 <MovieDataContainer>
-                    <Title>{popularMovie.title}</Title>
-                    <Year>{year}</Year>
+                    <Title>{/* {title} */} Mulan</Title>
+                    <Year>{/* {year} */} 2020</Year>
                     <MovieDate>
                         <MovieDateText>{/* {movieDateText} */}Production: </MovieDateText>
                         {/* {movieDate} */}China, United States of America
@@ -58,15 +58,15 @@ const MovieTileDetails = ({ id, poster, title, year, genre, rating, votes }) => 
                         {/* {movieDate} */}24.10.2020
                     </MovieDate>
                     <GenreTags>
-                        <Tag>{genre}</Tag>
-                        <Tag>{genre}</Tag>
-                        <Tag>{genre}</Tag>
+                        <Tag>{/* {genre} */}Action</Tag>
+                        <Tag>{/* {genre} */}Adventure</Tag>
+                        <Tag>{/* {genre} */}Drama</Tag>
                     </GenreTags>
                     <Stats>
                         <RatingIcon />
-                        <Rating>{rating}</Rating>
+                        <Rating>{/* {rating} */}7,8</Rating>
                         <RatingMax>{/* {ratingMax} */}/10</RatingMax>
-                        <Votes>{votes}</Votes>
+                        <Votes>{/* {votes} */}35 votes</Votes>
                     </Stats>
                     {isWideScreen && <DescriptionText>A young Chinese maiden disguises herself as a male warrior in order to save her father. Disguises herself as a male warrior in order to save her father.  A young Chinese maiden disguises herself as a male warrior in order to save her father.</DescriptionText>}
                 </MovieDataContainer>
