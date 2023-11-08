319c8a2a56a1d13e4085435d0a627c3ec5c1fce3
Angelika Mallek <angelika.mallek03@gmail.com>
2023-11-08 10:07:54 +0100

    remove bad files

--- src/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import popularMoviesReducer from "./features/movieSlice";
-
-export default configureStore({
- reducer: {
-    popularMovies: popularMoviesReducer,
- },
-});
