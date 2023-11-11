55d5538985a213022c3a9465c0d7dfd37901810d
Artur Feliks <arturfeliks4@gmail.com>
2023-11-11 14:45:40 +0100

    API to personPage added (without cast and crew)

--- src/features/personPage/PersonTile/index.js
+++ src/features/personPage/PersonTile/index.js
@@ -1,16 +1,17 @@
-import image from "../../../images/example-person.png";
-import { 
-    PersonTileContainer, 
-    Image, 
-    PersonDataContainer, 
+import noPhoto from "../../../images/no-person.svg";
+import {
+    PersonTileContainer,
+    Image,
+    PersonDataContainer,
     BirthText,
     BirthDate,
     DescriptionText,
 } from "./styled";
 import { NameContainer } from ".././styled";
 import { useState, useEffect } from "react";
+import { imageURL } from "../../../common/API/APIData";
 
-const PersonTile = ({ imagePerson, name, birthDate, birthPlace, description }) => {
+const PersonTile = ({ poster, name, birthDate, birthPlace, description }) => {
 
     const [isWideScreen, setIsWideScreen] = useState(window.innerWidth > 1140);
 
@@ -22,29 +23,37 @@ const PersonTile = ({ imagePerson, name, birthDate, birthPlace, description }) =
         window.addEventListener('resize', handleResize);
 
         return () => {
-			window.removeEventListener('resize', handleResize)
-		}
-    },[]);
+            window.removeEventListener('resize', handleResize)
+        }
+    }, []);
 
     return (
         <>
             <PersonTileContainer>
-                <Image src={image} alt="PersonImage"/> {/* {imagePerson */} 
+                {poster ? (
+                    <Image src={imageURL + poster} alt="Famous person" />
+                ) : (
+                    <Image src={noPhoto} alt="Logo" />
+                )}
                 <PersonDataContainer>
                     <NameContainer>
-                        Liu Yifei {/* {name */} 
+                        {name ? name : "Unknown name"}
                     </NameContainer>
                     <BirthDate>
                         <BirthText>Date of birth: </BirthText>
-                        25.08.1987 {/* {birthDate */} 
+                        {birthDate ? new Date(birthDate).toLocaleDateString(undefined, {
+                            day: '2-digit',
+                            month: '2-digit',
+                            year: 'numeric',
+                        }) : "Unknown"}
                     </BirthDate>
-                    <BirthDate> 
+                    <BirthDate>
                         <BirthText>Place of birth:  </BirthText>
-                        Wuhan, Hubei, China {/* {birthPlace */} 
+                        {birthPlace ? birthPlace : "Uknown"}
                     </BirthDate>
-                    {isWideScreen && <DescriptionText>{/* {description */}Liu Yifei was born in Wuhan, Hubei, Province of China on August 25th, 1987. She began modeling at the age of 8 and was trained in singing, dancing and the piano. Moving to the United States at 10 with her mother, Liu lived there for four years.</DescriptionText>}
+                    {isWideScreen && <DescriptionText>{description}</DescriptionText>}
                 </PersonDataContainer>
-                {!isWideScreen && <DescriptionText>{/* {description */}Liu Yifei was born in Wuhan, Hubei, Province of China on August 25th, 1987. She began modeling at the age of 8 and was trained in singing, dancing and the piano. Moving to the United States at 10 with her mother, Liu lived there for four years.</DescriptionText>}
+                {!isWideScreen && <DescriptionText>{description}</DescriptionText>}
             </PersonTileContainer>
         </>
     )
--- src/features/personPage/index.js
+++ src/features/personPage/index.js
@@ -3,15 +3,41 @@ import CastTile from "./CastTile";
 import CrewTile from "./CrewTile";
 import { NameContainer } from "./styled";
 import { GlobalWrapper } from "../../common/GlobalWrapper/styled";
+import { useParams } from "react-router-dom";
+import { usePopularPerson } from "./usePopularPerson";
+import Loading from "../../common/Loading";
+import Error from "../../common/Error";
 
-const Person = () => (
-    <GlobalWrapper>
-        <PersonTile />
-        <NameContainer>Movies - cast (4)</NameContainer>
-        <CastTile />
-        <NameContainer>Movies - crew (4)</NameContainer>
-        <CrewTile />
-    </GlobalWrapper>
-)
+const Person = () => {
+    const { id } = useParams();
+    const personId = id;
+
+    const { popularPerson, loading, error } = usePopularPerson(personId);
+
+    if (loading) {
+        return <Loading />;
+    }
+
+    if (error) {
+        return <Error />;
+    }
+
+    return (
+        <GlobalWrapper>
+            <PersonTile
+                key={popularPerson.id}
+                poster={popularPerson.profile_path}
+                name={popularPerson.name}
+                birthDate={popularPerson.birthday}
+                birthPlace={popularPerson.place_of_birth}
+                description={popularPerson.biography}
+            />
+            <NameContainer>Movies - cast (4)</NameContainer>
+            <CastTile />
+            <NameContainer>Movies - crew (4)</NameContainer>
+            <CrewTile />
+        </GlobalWrapper>
+    )
+};
 
 export default Person;
