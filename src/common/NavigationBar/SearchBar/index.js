6543a0abf61ebe5aacf6349fe489cbb69794a499
Michał Owsiak <micowsiak@gmail.com>
2023-11-12 17:42:09 +0100

    "Search for people" at movie details page fixed

--- src/common/NavigationBar/SearchBar/index.js
+++ src/common/NavigationBar/SearchBar/index.js
@@ -7,7 +7,7 @@ import { useLocation } from "react-router-dom";
 
 const SearchBar = () => {
 	const location = useLocation();
-	const moviesPage = location.pathname === "/movies";
+	const moviesPage = location.pathname.includes("/movies");
 
 	return (
 		<StyledSearchBar>
