e890f27aeb500d452973c0fe1f35bb0053b98b6a
Michał Owsiak <micowsiak@gmail.com>
2023-11-12 17:47:08 +0100

    unused reducer deleted

--- src/store.js
+++ src/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-// import popularMoviesReducer from "./features/movieSlice";
 import paginationReducer from "./common/Pagination/paginationSlice"
 
 export default configureStore({
     reducer: {
-        // popularMovies: popularMoviesReducer,
         pagination: paginationReducer,
     },
 });
