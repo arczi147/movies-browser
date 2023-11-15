152842b6b65c1c2d08ccf294b75506e627daadba
Michał Owsiak <micowsiak@gmail.com>
2023-11-16 00:17:19 +0100

    initial search engine implementation

--- src/common/NavigationBar/SearchBar/index.js
+++ src/common/NavigationBar/SearchBar/index.js
@@ -1,3 +1,4 @@
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import {
 	StyledSearchBar,
 	Input,
@@ -7,14 +8,50 @@ import { useLocation } from "react-router-dom";
 
 const SearchBar = () => {
 	const location = useLocation();
+	const history = useHistory();
 	const moviesPage = location.pathname.includes("/movies");
+	const query = (new URLSearchParams(location.search)).get("query");
+
+	const onInputChange = ({ target }) => {
+		const searchParams = new URLSearchParams(location.search);
+
+		if (target.value.trim() === "") {
+			searchParams.delete("query");
+
+			if (location.pathname.match(/\/(movies|people)\/\d+/)) {
+				const newPath = location.pathname.replace(
+					/\/(movies|people)\/\d+$/, `/${moviesPage ? "movies" : "people"}/search`
+				);
+				history.push(`${newPath}?${searchParams.toString()}`);
+			} else {
+				const newPath = location.pathname.replace(/\/search$/, "");
+				history.push(`${newPath}?${searchParams.toString()}`);
+			}
+		} else {
+			searchParams.set("query", target.value);
+
+			if (location.pathname.match(/\/(movies|people)\/\d+/)) {
+				const newPath = location.pathname.replace(
+					/\/(movies|people)\/\d+$/, `/${moviesPage ? "movies" : "people"}/search`
+				);
+				history.push(`${newPath}?${searchParams.toString()}`);
+			} else {
+				const newPath = `${location.pathname.replace(/\/search$/, "")}/search`;
+				history.push(`${newPath}?${searchParams.toString()}`);
+			}
+		}
+	};
 
 	return (
 		<StyledSearchBar>
 			<Icon />
-			<Input placeholder={
-				moviesPage ? "Search for movies..." : "Search for people..."
-			} />
+			<Input
+				placeholder={
+					moviesPage ? "Search for movies..." : "Search for people..."
+				}
+				value={query || ""}
+				onChange={onInputChange}
+			/>
 		</StyledSearchBar>
 	)
 };
