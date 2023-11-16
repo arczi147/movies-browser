d913da5abffe62e77d8b00088e3297fbcb920307
Michał Owsiak <micowsiak@gmail.com>
2023-11-16 19:01:13 +0100

    placeholder for pictures added

--- src/common/MovieTile/index.js
+++ src/common/MovieTile/index.js
@@ -2,6 +2,7 @@ import noPoster from "../../images/no-poster.svg"
 import { imageURL } from "../API/APIData";
 import {
 	Container,
+	Placeholder,
 	MovieDataContainer,
 	Poster,
 	Info,
@@ -17,52 +18,85 @@ import {
 	Rating,
 	Votes,
 } from "./styled";
+import { useState } from "react";
 
 function formatNumber(number) {
 	return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, "\u00A0");
 };
 
-const MovieTile = ({ id, poster, title, character, year, yearInParentheses, genre, rating, votes }) => (
-	<Container to={`/movies/${id}`}>
-		{poster ? (
-			<Poster src={imageURL + "w342" + poster} alt="Poster" />
-		) : (
-			<Poster src={noPoster} alt="Logo" />
-		)}
-		<MovieDataContainer>
-			<Info>
-				<Title>{title ? title : "Unknown title"}</Title>
-				<CharacterAndYearElements>
-					<Character>{character ? character : ""}</Character>
-					<Year>{year ? (new Date(year).getFullYear()) : ""}</Year>
-					<YearInParentheses>
-						{yearInParentheses ?
-							`(${(new Date(yearInParentheses).getFullYear())})` : ""}
-					</YearInParentheses>
-				</CharacterAndYearElements>
-				<GenreTags>
-					{genre ? (
-						genre.map((genreName, index) => (
-							<Tag key={index}>{genreName}</Tag>
-						))
-					) : ""}
-				</GenreTags>
-			</Info>
-			<Stats>
-				{votes ? (
+const MovieTile = ({
+	id,
+	poster,
+	title,
+	character,
+	year,
+	yearInParentheses,
+	genre,
+	rating,
+	votes
+}) => {
+
+	const [imageLoading, setImageLoading] = useState(true);
+
+	const handleImageLoaded = () => {
+		setImageLoading(false);
+	};
+
+
+	return (
+		(
+			<Container to={`/movies/${id}`}>
+				{poster ? (
 					<>
-						<RatingIcon />
-						<Rating>{rating ? rating.toFixed(1).replace(".", ",") : "0"}</Rating>
+						{imageLoading && (
+							<Placeholder />
+						)}
+						<Poster
+							src={imageURL + "w342" + poster}
+							alt="Poster"
+							onLoad={handleImageLoaded}
+							style={{ display: imageLoading ? "none" : "block" }}
+						/>
 					</>
 				) : (
-					""
+					<Poster src={noPoster} alt="Logo" />
 				)}
-				<Votes>
-					{votes ? formatNumber(votes) + " votes" : "No votes yet"}
-				</Votes>
-			</Stats>
-		</MovieDataContainer>
-	</Container>
-);
+				<MovieDataContainer>
+					<Info>
+						<Title>{title ? title : "Unknown title"}</Title>
+						<CharacterAndYearElements>
+							<Character>{character ? character : ""}</Character>
+							<Year>{year ? (new Date(year).getFullYear()) : ""}</Year>
+							<YearInParentheses>
+								{yearInParentheses ?
+									`(${(new Date(yearInParentheses).getFullYear())})` : ""}
+							</YearInParentheses>
+						</CharacterAndYearElements>
+						<GenreTags>
+							{genre ? (
+								genre.map((genreName, index) => (
+									<Tag key={index}>{genreName}</Tag>
+								))
+							) : ""}
+						</GenreTags>
+					</Info>
+					<Stats>
+						{votes ? (
+							<>
+								<RatingIcon />
+								<Rating>{rating ? rating.toFixed(1).replace(".", ",") : "0"}</Rating>
+							</>
+						) : (
+							""
+						)}
+						<Votes>
+							{votes ? formatNumber(votes) + " votes" : "No votes yet"}
+						</Votes>
+					</Stats>
+				</MovieDataContainer>
+			</Container>
+		)
+	)
+};
 
 export default MovieTile;
--- src/common/PersonTile/index.js
+++ src/common/PersonTile/index.js
@@ -1,23 +1,36 @@
-import { Container, PersonDataContainer, PersonPhoto, Name, Character } from "./styled";
+import React, { useState } from "react";
+import { Container, PersonDataContainer, PersonPhoto, Name, Character, Placeholder } from "./styled";
 import { imageURL } from "../API/APIData";
-import noPhoto from "../../images/no-person.svg"
+import noPhoto from "../../images/no-person.svg";
 
-const PersonTile = ({ id, name, character, poster }) => (
-    <Container to={`/people/${id}`}>
-        <PersonDataContainer>
-            {poster ? (
-                <PersonPhoto src={imageURL + "w185" + poster} alt="Famous person" />
-            ) : (
-                <PersonPhoto src={noPhoto} alt="Logo" />
-            )}
-            <Name>
-                {name}
-            </Name>
-            <Character>
-                {character}
-            </Character>
-        </PersonDataContainer>
-    </Container >
-);
+const PersonTile = ({ id, name, character, poster }) => {
+    const [imageLoading, setImageLoading] = useState(true);
+
+    const handleImageLoaded = () => {
+        setImageLoading(false);
+    };
+
+    return (
+        <Container to={`/people/${id}`}>
+            <PersonDataContainer>
+                {imageLoading && (
+                    <Placeholder />
+                )}
+                {poster ? (
+                    <PersonPhoto
+                        src={imageURL + "w185" + poster}
+                        alt="Famous person"
+                        onLoad={handleImageLoaded}
+                        style={{ display: imageLoading ? "none" : "block" }}
+                    />
+                ) : (
+                    <PersonPhoto src={noPhoto} alt="Logo" />
+                )}
+                <Name>{name}</Name>
+                <Character>{character}</Character>
+            </PersonDataContainer>
+        </Container>
+    );
+};
 
 export default PersonTile;
